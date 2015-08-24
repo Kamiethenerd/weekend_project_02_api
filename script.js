a1487970579e0919d479b62704168912d0791591
Kamiethenerd <Kamiethenerd@gmail.com>
2015-08-24 09:13:07 -0500

    hide api

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var apikey = 'ah8ab3w9skkcyqb3e438la75'; // Put your API key here
+var apikey = ''; // Put your API key here
 
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
@@ -22,21 +22,45 @@ $(document).ready(function() {
 	})
 });
 
-function randomThingGetter(){
-	// console.log(working);
-	$.ajax ({
-	    type: 'GET',
-	    dataType: 'json',
-	    crossDomain: true,
-	    url: 'https://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnlimit=1',
-	    complete: function() {
-	        console.log('ajax complete');
-	    },
-	    success: function(data) {
-	        searchCallback(data.results);
-	    },
-	    error: function(){
-	    	console.log('error!');
-	    }
-	});    
-};	
+xhr.setRequestHeader( 'Api-User-Agent', 'Example/1.0' );
+
+// Using jQuery
+$.ajax( {
+    url: remoteUrlWithOrigin,
+    data: queryData,
+    dataType: 'json',
+    type: 'POST',
+    headers: { 'Api-User-Agent': 'Example/1.0' },
+    success: function(data) {
+        searchCallback(data.results);
+    }
+} );
+
+// Using mw.Api, specify it when creating the mw.Api object
+var api = new mw.Api( {
+    ajax: {
+        url: 'https://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnlimit=1',
+        headers: { 'Api-User-Agent': 'Example/1.0' }
+    }
+} ).done(function(data) {
+    // do something with data
+});
+
+// function randomThingGetter(){
+// 	// console.log(working);
+// 	$.ajax(){
+// 	    type: 'GET',
+// 	    dataType: 'json',
+// 	    crossDomain: true,
+// 	    url: 'https://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnlimit=1',
+// 	    complete: function() {
+// 	        console.log('ajax complete');
+// 	    },
+// 	    success: function(data) {
+// 	        searchCallback(data.results);
+// 	    },
+// 	    error: function(){
+// 	    	console.log('error!');
+// 	    }
+// 	});    
+// };	
